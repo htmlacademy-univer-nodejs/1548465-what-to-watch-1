c39ceb3bb063f61bc88ab691f602962d44e1b7a1
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-09 06:23:15 +0500

    9. Add and registered import command

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,10 @@
 import VersionCommand from './cli-command/version-command.js';
 import HelpCommand from './cli-command/help-command.js';
 import CliApplication from './app/cli-application.js';
+import ImportCommand from './cli-command/import-command.js';
 
 const myManager = new CliApplication();
 myManager.registerCommands([
-  new HelpCommand, new VersionCommand
+  new HelpCommand, new VersionCommand, new ImportCommand
 ]);
 myManager.processCommand(process.argv);
