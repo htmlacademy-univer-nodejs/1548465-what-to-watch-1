6d8f0b1b5362de24ea4c81809cad8640d36ebcae
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-09 06:34:38 +0500

    11. Add some chalk style

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,10 +1,11 @@
+import chalk from 'chalk';
 import {CliCommandInterface} from './cli-command.interface';
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
-    console.log(`
+    console.log(chalk.blue(`
         Программа для подготовки данных для REST API сервера.
         Пример:
             main.js --<command> [--arguments]
@@ -13,6 +14,6 @@ export default class HelpCommand implements CliCommandInterface {
             --help:                      # печатает этот текст
             --import <path>:             # импортирует данные из TSV
             --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
-        `);
+        `));
   }
 }
