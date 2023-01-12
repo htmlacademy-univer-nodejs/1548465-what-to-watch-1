985d1709bc36dec43fade90945d270d7ff6e5761
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-13 01:54:21 +0500

    Little bug fix

--- src/common/file-writer/tsv-file-writer.ts
+++ src/common/file-writer/tsv-file-writer.ts
@@ -1,20 +1,20 @@
-import {FileWriterInterface} from './file-writer.interface.js';
-import {createWriteStream, WriteStream} from 'fs';
+import { createWriteStream, WriteStream} from 'fs';
+import { FileWriterInterface } from './file-writer.interface.js';
 
-export default class TsvFileWriter implements FileWriterInterface{
+export default class TSVFileWriter implements FileWriterInterface {
   private stream: WriteStream;
 
   constructor(public readonly filename: string) {
     this.stream = createWriteStream(this.filename, {
       flags: 'w',
       encoding: 'utf8',
-      highWaterMark: 2 ** 16,
+      highWaterMark: 2 ** 16, // 64KB
       autoClose: true,
     });
   }
 
   public async write(row: string): Promise<void> {
-    if(this.stream.write(`${row}\n`)) {
+    if (this.stream.write(`${row}\n`)) {
       return new Promise((resolve) => {
         this.stream.once('drain', () => resolve());
       });
