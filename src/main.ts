7609bd3c55e57fa72b6416704a5ab5311eac927d
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-14 00:29:45 +0500

    5. Updated DI container - add db service

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import {Component} from './types/component.types.js';
 import {ConfigInterface} from './common/config/config.interface.js';
 import {LoggerInterface} from './common/logger/logger.interface.js';
 import {Container} from 'inversify';
+import DatabaseService from './common/db-client/database.service.js';
+import {DatabaseInterface} from './common/db-client/database.interface.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
