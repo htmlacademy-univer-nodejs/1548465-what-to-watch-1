55cc24bca551b01715d751ad9c9c2b5c8ebedd95
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-14 00:17:03 +0500

    4. Add db service

---
+++ src/common/db-client/database.service.ts
@@ -0,0 +1,25 @@
+import {DatabaseInterface} from './database.interface.js';
+import {inject, injectable} from 'inversify';
+import {Component} from '../../types/component.types.js';
+import {LoggerInterface} from '../logger/logger.interface.js';
+import mongoose from 'mongoose';
+
+@injectable()
+export default class DatabaseService implements DatabaseInterface{
+  constructor(
+    @inject(Component.LoggerInterface) private logger: LoggerInterface,
+  ) {
+  }
+
+  public async connect(uri: string): Promise<void> {
+    this.logger.info('Try to connect MongoDB...');
+    await mongoose.connect(uri);
+    this.logger.info('Database connection established');
+  }
+
+  public async disconnect(): Promise<void> {
+    await mongoose.disconnect();
+    this.logger.info('Database connection closed.');
+  }
+
+}
