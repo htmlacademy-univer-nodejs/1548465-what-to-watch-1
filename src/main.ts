ac3ebfa6d511c3f423e366744b281f3482c7f018
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-14 19:09:19 +0500

    12. Add user service and DI

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,18 @@ import {LoggerInterface} from './common/logger/logger.interface.js';
 import {Container} from 'inversify';
 import DatabaseService from './common/db-client/database.service.js';
 import {DatabaseInterface} from './common/db-client/database.interface.js';
+import {UserServiceInterface} from './modules/user/user-service.interface.js';
+import UserService from './modules/user/dto/user.service.js';
+import {types} from '@typegoose/typegoose';
+import {UserEntity, UserModel} from './modules/user/user.entity.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+applicationContainer.bind<UserServiceInterface>(Component.UserServiceInterface).to(UserService).inSingletonScope();
+applicationContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
