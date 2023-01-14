1abd4dfdee4bb70b85a7e94405271f3d275c60af
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-15 03:51:59 +0500

    Add update interface for user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,6 +5,7 @@ import {UserEntity} from './user.entity.js';
 import {inject, injectable} from 'inversify';
 import {Component} from '../../types/component.types.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
+import UpdateUserDto from './dto/update-user.dto.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -30,5 +31,11 @@ export default class UserService implements UserServiceInterface {
   public async findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const existedUser = await this.findByEmail(dto.email);
     return existedUser ? existedUser : this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel
+      .findByIdAndUpdate(userId, dto, {new: true})
+      .exec();
   }
 }
