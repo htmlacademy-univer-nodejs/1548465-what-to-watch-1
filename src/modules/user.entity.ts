04556de66ef639a0eb6220533132699ca0e6bd92
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-14 18:31:40 +0500

    9. Add methods in user entity

--- src/modules/user.entity.ts
+++ src/modules/user.entity.ts
@@ -1,21 +1,46 @@
 import {User} from '../types/user.type.js';
-import typegoose, {defaultClasses, getModelForClass} from '@typegoose/typegoose';
+import typegoose, {defaultClasses, getModelForClass, modelOptions} from '@typegoose/typegoose';
+import {createSHA256} from '../utils/common.js';
 
 const {prop} = typegoose;
 
 export interface UserInterface extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users'
+  }
+})
+
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  @prop({unique: true, required: true})
-  public avatarPath!: string;
 
-  @prop()
-  public email!: string;
+  constructor(data: User) {
+    super();
 
-  @prop()
+    this.name = data.name;
+    this.email = data.email;
+    this.avatarPath = data.avatarPath;
+  }
+
+  @prop({required: true, default: ''})
   public name!: string;
 
-  @prop()
+  @prop({unique: true, required: true})
+  public email!: string;
+
+  @prop({required: true, default: ''})
+  public avatarPath!: string;
+
+  @prop({required: true, default: ''})
   public password!: string;
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import {Movie} from '../types/movie.type.js';
 import {Genre} from '../types/genre-type.enum.js';
 
@@ -27,3 +28,9 @@ export const createMovie = (row: string) => {
 
 export const getErrorMessage = (error: unknown) : string =>
   error instanceof Error ? error.message : '';
+
+
+export const createSHA256 = (line: string, salt: string): string => {
+  const shaHasher = crypto.createHmac('sha256', salt);
+  return shaHasher.update(line).digest('hex');
+};
