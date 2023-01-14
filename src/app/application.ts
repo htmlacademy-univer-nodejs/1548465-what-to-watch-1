54b8d1fcd48f52a29d55aaf5f7cb54a533fb9f16
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-15 04:36:58 +0500

    Init express application

--- src/app/application.ts
+++ src/app/application.ts
@@ -4,13 +4,18 @@ import {inject, injectable} from 'inversify';
 import {Component} from '../types/component.types.js';
 import {DatabaseInterface} from '../common/db-client/database.interface.js';
 import {getURI} from '../utils/db.js';
+import express, {Express} from 'express';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
-    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface) {
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
+  ) {
+    this.expressApp = express();
   }
 
   public async init() {
@@ -24,6 +29,9 @@ export default class Application {
       this.config.get('DB_PORT'),
       this.config.get('DB_NAME'),
     );
+
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
 
     await this.databaseClient.connect(uri);
   }
