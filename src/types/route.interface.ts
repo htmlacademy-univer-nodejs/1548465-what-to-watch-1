4cdae433b86c65639904158cf770e0958e298ce1
AydartsyanGrisha <aydartsyang@gmail.com>
2023-01-16 05:00:06 +0500

    Little fix

--- src/types/route.interface.ts
+++ src/types/route.interface.ts
@@ -6,6 +6,5 @@ export interface RouteInterface {
   path: string;
   method: HttpMethod;
   handler: (req: Request, res: Response, next: NextFunction) => void;
-
   middlewares?: MiddlewareInterface[];
 }
